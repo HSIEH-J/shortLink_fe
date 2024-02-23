29366e9ad63910484ec9e291b6bf17d6a81b1ee8
YICHEN HSIEH <boisw3641@gmail.com>
2024-02-23 16:58:53 +0800

    result table

--- src/pages/homePage/HomPage.tsx
+++ src/pages/homePage/HomPage.tsx
@@ -1,6 +1,39 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 //Import react router dom
 import { useNavigate } from 'react-router-dom';
+
+type ButtonProps =  {
+    onClick: MouseEventHandler<HTMLButtonElement>,
+    text: string,
+};
+
+type ResultLists = {
+    originalURL: string,
+    shortURL: string,
+    redirectTimes: number,
+};
+
+type Result = {
+    lists: ResultLists[],
+};
+
+const fakeData: ResultLists[] = [
+    {
+        originalURL: "www.https://longlonglong1.com.tw",
+        shortURL: "www.https://short1.com.tw",
+        redirectTimes: 100,
+    },
+    {
+        originalURL: "www.https://longlonglong2.com.tw",
+        shortURL: "www.https://short2.com.tw",
+        redirectTimes: 100,
+    },
+    {
+        originalURL: "www.https://longlonglong3.com.tw",
+        shortURL: "www.https://short3.com.tw",
+        redirectTimes: 100,
+    }
+];
 
 const HomePage: React.FC = () => {
     const navigate = useNavigate();
@@ -16,25 +49,43 @@ const HomePage: React.FC = () => {
     return (
         <div>
             <div>
-                <button onClick={onLogin}>Log in</button>
-                <button onClick={onSignUp}>Sign up</button>
+                <Button onClick={onLogin} text={"login"} />
+                <Button onClick={onSignUp} text={"Sign up"} />
             </div>
             <div>
-                
+                <ResultTable lists={fakeData} />
             </div>
         </div>
     );
 };
 
-const ResultTable: React.FC = ({ data }) => {
-    const content = data.map((item) => (<td>{item}</td>));
+const Button: React.FC<ButtonProps> = ({onClick, text}) => {
+    return (
+        <button onClick={onClick}>{text}</button>
+    );
+};
+
+const ResultTable: React.FC<Result> = ({ lists }) => {
+    const content = lists.map((item) => {
+        return(
+            <tr>
+                <td>{item.originalURL}</td>
+                <td>{item.shortURL}</td>
+                <td>{item.redirectTimes}</td>
+            </tr>
+        )
+    });
     return (
         <table>
-            <thead>Results</thead>
-            <tbody>
+            <thead>
                 <tr>
-                    {content}
+                    <th>URL</th>
+                    <th>Short URL</th>
+                    <th>Redirect Times</th>
                 </tr>
+            </thead>
+            <tbody>
+                {content}
             </tbody>
         </table>
     );
---
+++ src/utils/fakeData.tsx
@@ -0,0 +1,21 @@
+export const fakeData = [
+    {
+        originalURL: "www.https://longlonglong1.com.tw",
+        shortURL: "www.https://short1.com.tw",
+        redirectTimes: 100,
+    },
+    {
+        originalURL: "www.https://longlonglong2.com.tw",
+        shortURL: "www.https://short2.com.tw",
+        redirectTimes: 100,
+    },
+    {
+        originalURL: "www.https://longlonglong3.com.tw",
+        shortURL: "www.https://short3.com.tw",
+        redirectTimes: 100,
+    }
+];
+
+
+
+
